679ef4388c208aa923432b189742554793465d1f
unknown <hamidbit99@gmail.com>
2026-08-02 10:54:52 +0330

    vercel-commit-one

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../src/generated/prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
 import { PrismaLibSql } from "@prisma/adapter-libsql";
 import * as bcrypt from "bcryptjs";
 
